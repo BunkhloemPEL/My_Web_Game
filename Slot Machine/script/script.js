1e23f1fd58cffcf12018ef13ca74e8f9be71bbe0
Bunkhloem PEL <mrr0045khloem@gmail.com>
2024-09-09 18:35:02 +0700

    Complete Functionality of the Machine

--- Slot Machine/script/script.js
+++ Slot Machine/script/script.js
@@ -16,4 +16,34 @@ function spin() {
     return result
 }
 
-console.log(spin())
+function prize(result) {
+    if (JSON.stringify(result) == JSON.stringify(["Diamon","Diamon","Diamon"])){
+        return 200
+    }
+    if (JSON.stringify(result) == JSON.stringify(["3 Bar","3 Bar","3 Bar"])){
+        return 40
+    }
+    if (JSON.stringify(result) == JSON.stringify(["2 Bar","2 Bar","2 Bar"])){
+        return 25
+    }
+    if (JSON.stringify(result) == JSON.stringify(["1 Bar","1 Bar","1 Bar"])){
+        return 10
+    }
+    if (JSON.stringify(result) == JSON.stringify(["Cherry","Cherry","Cherry"])){
+        return 10
+    }
+    if (result.includes("Cherry")){
+        if(result.filter(item => item === "Cherry").length == 2){
+            return 5
+        }else{
+            return 2
+        }
+    }
+    else{
+        return 0
+    }
+}
+
+result = spin()
+console.log(result)
+console.log(prize(result))
